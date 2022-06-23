0e3c4004a4ef08adb5485ae89f9bdbcca0cf71ea
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-23 14:10:35 +0530

    Display maps

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,39 @@
+/* eslint-disable react/function-component-definition */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDataFromApi } from '../redux/person';
+import Map from './Map';
 
-function Home() {
+const Home = () => {
   const dispatch = useDispatch();
   const personStore = useSelector((store) => store.person);
+  const markers = [];
 
   useEffect(() => {
     dispatch(getDataFromApi());
   }, []);
 
-  console.log(personStore);
+  if (personStore.length > 0) {
+    personStore.forEach((obj) => {
+      const data = obj.data.person;
+      data.forEach((item) => {
+        const mark = {};
+        mark.coordinates = [item.lat, item.long];
+        mark.name = item.name;
+        mark.id = item.id;
+        markers.push(mark);
+      });
+    });
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <p>Home</p>
+      {
+        markers.length > 0 ? <Map markers={markers} /> : <p>Maps not available currently</p>
+      }
+    </div>
   );
-}
+};
 
 export default Home;
