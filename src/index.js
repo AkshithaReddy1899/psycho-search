5b1bcd7cd34b47db2a722a3b3730b9e8e0a6b8aa
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-07-26 18:47:09 +0530

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 import store from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,6 +15,9 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
+
+ReactGA.initialize('UA-235638220-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
